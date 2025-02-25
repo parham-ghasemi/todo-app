e920aede278cf3b1e36fc31526e57b7964bf08b8
parham-ghasemi <parham.ghasemi.1388@gmail.com>
2025-02-26 01:28:32 +0330

    Added initial ui

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-const { app, BrowserWindow } = require('electron/main')
+import { app, BrowserWindow } from 'electron'
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
   })
 
   win.loadFile('index.html')
